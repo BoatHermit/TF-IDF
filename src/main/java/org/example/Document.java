341ae4f02a60b777d0aa5ef9277976f9980529be
赵希雯 <201250083@smail.nju.edu.cn>
2023-11-16 18:13:49 +0800

    初步完成计算TF

--- src/main/java/org/example/Document.java
+++ src/main/java/org/example/Document.java
@@ -22,7 +22,8 @@ public class Document {
     List<Term> terms = new ArrayList<>();
 
     Map<String, Integer> termMap = new HashMap<>();
-    Map<String, Integer> IF_IDFMap = new HashMap<>();
+    Map<String, Double> TFMap = new HashMap<>();
+    Map<String, Double> IF_IDFMap = new HashMap<>();
 
 
     /**
@@ -40,4 +41,30 @@ public class Document {
         return 0;
     }
 
+    /**
+     * 计算TF
+     * @param standard 使用哪种标准进行计算，true表示除数为term总数
+     */
+    public void calTF(boolean standard) {
+        int maxFre = 0;
+        for (Term term : terms) {
+            String word = term.toString();
+            if(termMap.get(word) == null){
+                termMap.put(word, 1);
+            } else{
+                termMap.put(word, termMap.get(word) + 1);
+                maxFre = Math.max(maxFre, termMap.get(word));
+            }
+        }
+        int wordLen = terms.size();
+        for (String word : termMap.keySet()) {
+            double tf;
+            if (standard) {
+                tf = (double) termMap.get(word) / wordLen;
+            } else {
+                tf = (double) termMap.get(word) / maxFre;
+            }
+            TFMap.put(word, tf);
+        }
+    }
 }
