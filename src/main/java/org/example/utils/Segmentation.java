aa67a80a48df97f6da90d9461239d1b4e7441279
jiangnan <201250033@smail.nju.edu.cn>
2023-12-31 15:56:57 +0800

    基本完成停用词过滤

--- src/main/java/org/example/utils/Segmentation.java
+++ src/main/java/org/example/utils/Segmentation.java
@@ -1,5 +1,9 @@
 package org.example.utils;
 
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.dictionary.stopword.CoreStopWordDictionary;
+import com.hankcs.hanlp.dictionary.stopword.StopWordDictionary;
+import com.hankcs.hanlp.seg.common.Term;
 import com.hankcs.hanlp.tokenizer.NLPTokenizer;
 import org.example.model.vo.Document;
 import org.example.model.vo.ExternalFile;
@@ -22,13 +26,20 @@ public class Segmentation {
 //        HanLPClient HanLP = new HanLPClient("https://www.hanlp.com/api", "MzM3MkBiYnMuaGFubHAuY29tOjdxbm0wYzZ2dWxVekROeHY=", "zh", 10); // auth不填则匿名，zh中文，mul多语种
         List<Document> externalRegulations = new ArrayList<>();
         //外规文件分词
+        HanLP.Config.ShowTermNature = false;//去除词性
         for (ExternalFile file : externalFiles){
             Document document = new Document();
             document.setId(file.getId());
             document.setInternal(false);
             document.setTitle(file.getTitle());
             document.setDepartment(null);
-            document.setTerms(NLPTokenizer.segment(file.getContent()));
+            //选择停用词表
+//            CoreStopWordDictionary.load("src/main/resources/stopwords/baidu_stopwords.txt", false);
+//            CoreStopWordDictionary.load("src/main/resources/stopwords/cn_stopwords.txt", false);
+//            CoreStopWordDictionary.load("src/main/resources/stopwords/hit_stopwords.txt", false);
+//            CoreStopWordDictionary.load("src/main/resources/stopwords/scu_stopwords.txt", false);
+            //分词
+            document.setTerms(CoreStopWordDictionary.apply(NLPTokenizer.segment(file.getContent())));
             externalRegulations.add(document);
         }
         return externalRegulations;
@@ -43,13 +54,20 @@ public class Segmentation {
 //        HanLPClient HanLP = new HanLPClient("https://www.hanlp.com/api", "MzM3MkBiYnMuaGFubHAuY29tOjdxbm0wYzZ2dWxVekROeHY=", "zh", 10); // auth不填则匿名，zh中文，mul多语种
         List<Document> internalRegulations = new ArrayList<>();
         //内规文件分词
+        HanLP.Config.ShowTermNature = false;//去除词性
         for (InternalFile file : internalFiles){
             Document document = new Document();
             document.setId(file.getId());
             document.setInternal(true);
             document.setTitle(file.getTitle());
             document.setDepartment(file.getDepartment());
-            document.setTerms(NLPTokenizer.segment(file.getContent()));
+            //选择停用词表
+//            CoreStopWordDictionary.load("src/main/resources/stopwords/baidu_stopwords.txt", false);
+//            CoreStopWordDictionary.load("src/main/resources/stopwords/cn_stopwords.txt", false);
+//            CoreStopWordDictionary.load("src/main/resources/stopwords/hit_stopwords.txt", false);
+//            CoreStopWordDictionary.load("src/main/resources/stopwords/scu_stopwords.txt", false);
+            //分词
+            document.setTerms(CoreStopWordDictionary.apply(NLPTokenizer.segment(file.getContent())));
             internalRegulations.add(document);
         }
         return internalRegulations;
